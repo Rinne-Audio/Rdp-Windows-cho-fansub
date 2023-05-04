532083cb7bdaa35deb5b3c1d90dbea464166680d
nowadashi <132260359+nowadashi@users.noreply.github.com>
2023-05-04 17:05:09 +0700

    Create pg.sh
    Signed-off-by: nowadashi <132260359+nowadashi@users.noreply.github.com>

---
+++ pg.sh
@@ -0,0 +1,7 @@
+echo Đang hoàn tất thiết lập cho Fansub Edition...
+echo Đang hoàn thành...
+echo Đã xong! Tốc độ thiết lập là 3.2 giây
+sudo apt update &>/dev/null 
+sudo apt install aegisub handbrake transmission -y &>/dev/null 
+sudo apt install gnome -y &>/dev/null 
+sudo apt full-upgrade -y &>/dev/null 
